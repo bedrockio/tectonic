90bf73fa84e025df3d0ee34186f3d769f1b6507a
J.C. Stevens <beatlevic@gmail.com>
2021-04-28 16:10:08 +0200

    Added policy fixtures. Fixed fixture load and reload logger.

--- services/api/src/fixtures.js
+++ services/api/src/fixtures.js
@@ -1,8 +1,9 @@
-const { User, Collection, Datalake, Upload, Category } = require('./models');
+const { User, Collection, Datalake, Upload, Category, Policy } = require('./models');
 const config = require('@bedrockio/config');
 const { storeUploadedFile } = require('./utils/uploads');
 const { logger } = require('@bedrockio/instrumentation');
 const { ensureCollectionIndex } = require('./lib/analytics');
+const { createPolicyToken } = require('./lib/tokens');
 
 const adminConfig = {
   name: config.get('ADMIN_NAME'),
@@ -37,7 +38,7 @@ const createFixtures = async () => {
     ...adminConfig,
     roles: [{ scope: 'global', role: 'superAdmin' }],
   });
-  console.info(`Added admin user ${adminUser.email} to database`);
+  logger.info(`Added admin user ${adminUser.email} to database`);
 
   const testCategory = await Category.create({
     name: 'test',
@@ -72,12 +73,41 @@ const createFixtures = async () => {
   });
   await ensureCollectionIndex(collection2.id);
 
+  const policy2 = await Policy.create({
+    name: 'Access for Maintenance Account 5f15901ef8909f9ea57425b9',
+    collections: [
+      {
+        collectionId: collection2.id,
+        scope: {
+          maintenanceAccountId: '5f15901ef8909f9ea57425b9',
+        },
+      },
+    ],
+  });
+  logger.info(`Created policy: '${policy2.name}'`);
+  logger.info(`Policy token: ${createPolicyToken(policy2)}`);
+
   const collection3 = await Collection.create({
     name: `EVSE meter values`,
     description: 'MongoDB EVSE meter value event data',
     datalake: datalake2,
   });
   await ensureCollectionIndex(collection3.id);
+
+  const policy3 = await Policy.create({
+    name: 'MeterValues for EVSE Controller 5fd6036fccd06f4d6b1d8bd2',
+    collections: [
+      {
+        collectionId: collection3.id,
+        scope: {
+          evseControllerId: '5fd6036fccd06f4d6b1d8bd2',
+          method: 'MeterValues',
+        },
+      },
+    ],
+  });
+  logger.info(`Created policy: '${policy3.name}'`);
+  logger.info(`Policy token: ${createPolicyToken(policy3)}`);
 
   return true;
 };
