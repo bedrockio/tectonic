a39e9b5c132d0f339bd5d66e42dec799490d0d62
Kaare Larsen <me@kaareal.com>
2021-06-15 16:32:52 +0200

    hide tab for stats as we dont have that working yet, cant be coming from the analytics api, to to access

--- services/web/src/screens/Collections/Detail/index.js
+++ services/web/src/screens/Collections/Detail/index.js
@@ -78,7 +78,7 @@ export default class CollectionDetail extends React.Component {
         <Route exact path="/collections/:id" render={(props) => <Overview {...props} {...this.state} />} />
         {/* --- Generator: routes */}
         <Route exact path="/collections/:id/batches" render={(props) => <Batches {...props} {...this.state} />} />
-        <Route exact path="/collections/:id/stats" render={(props) => <Stats {...props} {...this.state} />} />
+        {/* <Route exact path="/collections/:id/stats" render={(props) => <Stats {...props} {...this.state} />} /> */}
         {/* --- Generator: end */}
       </Switch>
     );
