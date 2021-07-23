f812251987f0af841d0fd854b28f9a09e26d99a8
J.C. Stevens <beatlevic@gmail.com>
2021-07-23 18:08:48 +0200

    Fix for checking against collection lastEntryAt

--- services/api/src/routes/events.js
+++ services/api/src/routes/events.js
@@ -8,6 +8,7 @@ const { memorySizeOf, chunkedAsyncMap } = require('../lib/events');
 const { storeBatchEvents } = require('../lib/batch');
 const { createHash } = require('crypto');
 const { authenticate, fetchCredential } = require('../lib/middleware/authenticate');
+const { logger } = require('@bedrockio/instrumentation');
 
 const PUBSUB_RAW_EVENTS_TOPIC = config.get('PUBSUB_RAW_EVENTS_TOPIC');
 const EVENTS_CHUNK_SIZE = 10;
@@ -110,12 +111,15 @@ router
       // Update collection.lastEntryAt if timeField is set
       const { timeField, lastEntryAt } = collection;
       if (timeField) {
-        let maxTimeFieldValue = lastEntryAt;
+        let maxTimeFieldValue = '';
         for (const event of events) {
           if (event[timeField] && event[timeField] > maxTimeFieldValue) maxTimeFieldValue = event[timeField];
         }
-        if (maxTimeFieldValue > lastEntryAt) {
-          // Update collection lastEntryAt atomically
+        if (!lastEntryAt && maxTimeFieldValue) {
+          logger.info(`Setting lastEntryAt for collection ${collection.name} to: ${maxTimeFieldValue}`);
+          await Collection.findOneAndUpdate({ _id: collection.id }, { lastEntryAt: maxTimeFieldValue });
+        } else if (maxTimeFieldValue > lastEntryAt.toISOString()) {
+          logger.info(`Updating lastEntryAt for collection ${collection.name} to: ${maxTimeFieldValue}`);
           await Collection.findOneAndUpdate(
             { _id: collection.id, lastEntryAt: { $lt: maxTimeFieldValue } },
             { lastEntryAt: maxTimeFieldValue }
